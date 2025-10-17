29c7bf7e6aa5839f528a5cfcded8a85f890e597d
Merc <sulfur-sorcerer@sulfur-sorcerer.merc>
2025-10-17 05:50:01 +0530

    Version 2.0 release: major update and improvements

--- cli_arg_proc.c
+++ cli_arg_proc.c
@@ -3,17 +3,17 @@
 /**
  * @brief Variable initialization
  */
-const char* cli_arg_table[CLI_ARG_COUNT]={ADDACCOUNT};
-const int (*fptr_arr[CLI_ARG_COUNT])(void)={add_account};
+const char* cli_arg_table[CLI_ARG_COUNT]={ARG1, ARG2, ARG3};
+const int cli_args_size[CLI_ARG_COUNT]={SIZEOF_CSTR(ARG1), SIZEOF_CSTR(ARG2), SIZEOF_CSTR(ARG3)};
+const int (*fptr_arr[CLI_ARG_COUNT])(void*)={func_arg1, func_arg2, func_arg3};
 
 /**
  * @brief Adds more google drive account for uploading to different drives
  * @return returns 0 on success
  */
-int add_account(void){
-	return 0;
-
-}
+int func_arg1(void*){ printf("Hello from func_arg1\n"); return 0; }
+int func_arg2(void*){ printf("Hello from func_arg2\n"); return 0; }
+int func_arg3(void*){ printf("Hello from func_arg3\n"); return 0; }
 
 /**
  * more functions here....
@@ -29,75 +29,115 @@ int add_account(void){
  * @param param2 passes the pointer to the argument string array
  * @return returns 0 on success else failed
  */
-int cli_arg_dispatcher(int argc, char** argv){
+int cli_arg_dispatcher_lib(int argc, char** argv){
 	// need to read from the second argument since first one always the name of the program itself.
 	if(argc<=1){
 		return 0;
 	}
+
+/*
 	if((argc-1)>CLI_ARG_COUNT){
 		printf("Maximum argument supported : %d, Given : %d\n", CLI_ARG_COUNT, argc-1);
 		return EXTRAARG;
 	}
-	
+*/	
+	printf("relation array being created...\n");
+
+	char relation[argc-1];
+        memset(relation, 0x00, argc-1);	
+	// this array's elements in sequence represents the index in cli_arg_table and the values stored in the array represents the index in argv
+
+
+
 	// let's first validate all the CLI args to avoid any inconsistent program state if any one of the cli arg is wrong in multi arg passing, while validating, we'll store the index of the matched string so that later we can just lookup and dispatch without re-parsing the entire CLI args with cli_arg_table
-	unsigned int cli_arg_index=0x00000000;
-	unsigned int cli_arg_index_dupl=0x00000000;
-	char flag0=0;
-	char flag1=0;
-	char duplic_flag=0;
-	for(int i=1;i<argc;i++){
-		flag0=0;
+
+	char valid_arg_tracer=0;
+	char unknown_arg_tracer=0;
+	char dupl_arg_found_flag=0;
+	for(int i=0;i<argc-1;i++){
+		printf("parsing argv[%d] : %s ...\n",i+1,argv[i+1]);
+		valid_arg_tracer=0;
 		for(int j=0;j<CLI_ARG_COUNT;j++){
-			if(strcmp(argv[i], cli_arg_table[j])==0){
-				// CLI arg string matched, i_th arg correct.
-				flag0=1;
+			printf("comparing argv[%d] %s with cli_arg_table[%d] %s...\n", i+1, argv[i+1], j, cli_arg_table[j]);
+			if(strncmp(argv[i+1], cli_arg_table[j], cli_args_size[j])==0){
+				// CLI arg string matched, i_th arg from the list of cli provided arg list is correct.
+				valid_arg_tracer=1;
 				// let's check if corresponding bit is already set, if yes this means CLI arg duplication else not.
-				if(((cli_arg_index)&(1<<j))==0){
+				if((relation[j])==0){
 					// no duplication
-					cli_arg_index|=(1<<j);
+					relation[j]=i+1;
+                   			 // since i_th arg matched, i_th bit is set in index_in_cli_arg_table
+                     
 				}else{
 					// arg duplication, setting flag;
-					duplic_flag++;
-					// setting cli_arg_index_dupl
-					cli_arg_index_dupl|=(1<<j);
+					dupl_arg_found_flag++;
+					// setting index_in_cli_arg_table_dupl
+					if(relation[j]>0){ relation[j]=(-1)*relation[j]; }
 				}	
 				
 				break;
 			}
 		}
-		if(flag0!=1){	
+		if(valid_arg_tracer!=1){	
 		// entire array searched, but CLI arg doesnt matched, thus invalid CLI arg
-			printf("Invalid argument %d : %s\n",i,argv[i]);
-			flag1++;
+			printf("Unknown argument %d : %s\n", i+1, argv[i+1]);
+			unknown_arg_tracer++;
 		}
 	}
-	if(flag1!=0){
-		printf("Total invalid arguments : %d\n",flag1);
+	if(unknown_arg_tracer!=0){
+		printf("Total unknown arguments : %d\n",unknown_arg_tracer);
 		printf("Accepted CLI arguments are listed below :\n[\n");
 		for(int k=0;k<CLI_ARG_COUNT;k++){
 			printf("[*] %s\n",cli_arg_table[k]);
 		}
 		printf("]\n");
-		return EINVALARG;
+		// return EINVALARG;
 	}
-	if(duplic_flag!=0){
+	if(dupl_arg_found_flag!=0){
 		printf("Duplicated argument found : \n");
 		for(int l=0;l<CLI_ARG_COUNT;l++){
-			if(((cli_arg_index_dupl)&(1<<l))!=0){
+			if((relation[l]<0)){
 				printf("%s\n",cli_arg_table[l]);
 			}
 		}
 		return EDUPLARG;
 	}
 
-	// all CLI args parsed, corresponding indices are stored in cli_arg_index
-	for(int m=0;m<CLI_ARG_COUNT;m++){
-		if(((cli_arg_index)&(1<<m))!=0){
-			// corresponding bit found, executing the handler.
-			if((fptr_arr[m])()!=0){
-				printf("Handler at index %d and address %p failed.\n",m,fptr_arr[m]);
-				return EHANDLER;
+	printf("dispatching...\n");
+	
+	
+	for(int p=0;p<CLI_ARG_COUNT;p++){
+		printf("relation[%d] : %d\n", p, relation[p]);
+	}
+	
+	// all CLI args parsed, corresponding indices are stored in index_in_cli_arg_table
+	for(int m=0;m<argc-1;m++){
+		if(relation[m]>0){
+
+			printf("current iter index in argv : %d\n", m);
+			// corresponding index in argv found, executing the handler.
+			// now checking whether the provided arg is switch or a variable type i.e. -switch or -var=X
+
+			if(strlen(argv[relation[m]])==cli_args_size[m]){
+				// its a switch, executing handler as is
+				printf("dispatching handler for switch %s...\n", argv[relation[m]]);
+				if((fptr_arr[m])(NULL)!=0){
+					printf("Handler ar index %d and address %p failed.\n", m, fptr_arr[m]);
+					return EHANDLER;
+				}
+				continue;
 			}
+			
+			// its not a switch, value based arg i.e. -var=X
+			
+			printf("dispatching handler for var_arg %s...\n", argv[relation[m]]);
+			if(((fptr_arr[m])((void*)(argv[relation[m]]+cli_args_size[m]+1) )!=0)){
+				// +1 to make the pointer to point beyond '=' i.e. to next char whatever it is
+				printf("Handler at index %d and address %p failed.\n", m, fptr_arr[m]);
+			
+			}
+
+
 		}
 	}
 
--- cli_arg_proc.h
+++ cli_arg_proc.h
@@ -5,8 +5,7 @@
  * @file cli_arg_proc.h
  * @brief Declares prototypes for command line argument handling, let's have a brief look at how the CLI args 
  * 	  are passed to the program :
- * 	  Any option passed must be of form '-arg' , if arg supports an input value as well, program will ask 
- * 	  for it during runtime.
+ * 	  Any option passed must be of form '-arg' and for input with argument, use the form '-arg=X', X can be anything like float or string or int etc
  * 	  All CLI args in details are mentioned below :
  * 	  [#] -addaccount : Adds more google drive accounts by asking for new json file path.
  * 	  [#]	// more to be added....
@@ -23,9 +22,14 @@
  */
 // General macros
 #define MAX_CLI_ARGS	32
-#define CLI_ARG_COUNT	1
+#define CLI_ARG_COUNT	3
 
-#define ADDACCOUNT "-addaccount"
+// Arg macros
+#define ARG1    "-arg1"
+#define ARG2    "-arg2"
+#define ARG3    "-arg3"
+
+#define SIZEOF_CSTR(x) (sizeof(x)-(1))  // excluding the null terminator from string length to get char only size 
 
 // cli_arg_dispatcher :
 #define EXTRAARG	-1
@@ -37,13 +41,15 @@
  * @brief Variable declarations
  */
 extern const char* cli_arg_table[CLI_ARG_COUNT];
-extern const int (*fptr_arr[CLI_ARG_COUNT])(void); // array of function pointers
+extern const int (*fptr_arr[CLI_ARG_COUNT])(void*); // array of function pointers
 
 /**
  * @brief Adds more google drive account for uploading to different drives
  * @return returns 0 on success
  */
-int add_account(void);
+int func_arg1(void*);
+int func_arg2(void*);
+int func_arg3(void*);
 
 /**
  *
@@ -57,7 +63,7 @@ int add_account(void);
  * @param param2 passes the pointer to the argument string array
  * @return returns 0 on success else failed
  */
-int cli_arg_dispatcher(int argc, char** argv);
+int cli_arg_dispatcher_lib(int argc, char** argv);
 
 #endif /**< __CLI_ARG_PROC_H__ */
 
